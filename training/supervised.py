0fc64564590e129e259dd8c1b514f6b74aad6d41
sarrkar <140555126+sarrkar@users.noreply.github.com>
2024-03-28 15:08:50 +0330

    fixed minor bugs

--- training/supervised.py
+++ training/supervised.py
@@ -3,12 +3,15 @@ from model.mlp import LinearClassifier
 from datasets.cifar import get_cifar10
 from evaluation.top1 import Top1
 
-from tqdm import tqdm, trange
+import numpy as np
+
+from tqdm.auto import tqdm, trange
 
 from torchvision.transforms import ToTensor
 
 import torch
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 class SupervisedClassifier:
@@ -18,8 +21,8 @@ class SupervisedClassifier:
             dataset=get_cifar10,
             classifier=LinearClassifier,
             evaluation=Top1,
-            epochs=100,
-            lr=0.001,
+            epochs=30,
+            lr=0.01,
             device='cuda'
     ):
         self.device = torch.device(device)
@@ -30,9 +33,13 @@ class SupervisedClassifier:
         self.test_dataloader = DataLoader(
             self.test_dataset, batch_size=32, shuffle=False)
         self.classifier = classifier(
-            self.backbone.output_size, self.dataset_train.num_classes)
+            self.backbone.output_size, 
+            len(self.dataset_train.classes)
+        ).to(self.device)
         self.epochs = epochs
-        self.optimizer = torch.optim.Adam(self.classifier.parameters(), lr=lr)
+        params = list(self.backbone.parameters()) + list(self.classifier.parameters())
+        self.optimizer = torch.optim.Adam(params, lr=lr)
+        self.scheduler = ReduceLROnPlateau(self.optimizer, 'min', patience=250, factor=0.5)
         self.criterion = torch.nn.CrossEntropyLoss()
         self.evaluation = evaluation()
 
@@ -40,6 +47,7 @@ class SupervisedClassifier:
         self.backbone.train()
         self.classifier.train()
         for epoch in trange(self.epochs):
+            losses = []
             pbar = tqdm(self.train_dataloader)
             for x, y in pbar:
                 x, y = x.to(self.device), y.to(self.device)
@@ -49,7 +57,9 @@ class SupervisedClassifier:
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-                pbar.set_description(f'EPOCH {epoch + 1} - LOSS {loss.item()}')
+                self.scheduler.step(loss)
+                losses.append(loss.item())
+                pbar.set_description(f'EPOCH {epoch + 1} - LOSS {np.mean(losses):.4f}')
 
     def test(self):
         self.backbone.eval()
